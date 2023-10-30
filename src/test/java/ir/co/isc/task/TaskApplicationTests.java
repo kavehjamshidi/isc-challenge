1bb0cbf5fd54c14d876d92d156931fc8a3c57355
Kaveh Jamshidi <kaveh.jamshidi98@gmail.com>
2023-10-30 17:04:29 +0330

    initial commit

---
+++ src/test/java/ir/co/isc/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.co.isc.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
