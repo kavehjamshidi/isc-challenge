9e4183c28f58d9f1ef1109f1b46e56c5e9d1a61e
Kaveh Jamshidi <kaveh.jamshidi98@gmail.com>
2023-11-02 04:02:33 +0330

    added project lombok, added timestamps to entities

--- src/main/java/ir/co/isc/task/domain/Professor.java
+++ src/main/java/ir/co/isc/task/domain/Professor.java
@@ -1,10 +1,15 @@
 package ir.co.isc.task.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 public class Professor {
 
@@ -14,70 +19,12 @@ public class Professor {
     private String firstName;
     private String lastName;
     private String nationalId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "professor")
     private Set<Course> courses = new HashSet<>();
-
-    public Set<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(Set<Course> courses) {
-        this.courses = courses;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getNationalId() {
-        return nationalId;
-    }
-
-    public void setNationalId(String nationalId) {
-        this.nationalId = nationalId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Professor professor)) return false;
-
-        return getId() != null ? getId().equals(professor.getId()) : professor.getId() == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return getId() != null ? getId().hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Professor{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", nationalId='" + nationalId + '\'' +
-                '}';
-    }
 }
